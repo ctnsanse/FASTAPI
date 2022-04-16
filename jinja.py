5eefa66a87aa3d86ae11d8f02d74ded42e0a442f
Ctnsanse <cleytonsanse@gmail.com>
2022-04-16 19:37:57 +0200

    jinja

---
+++ jinja.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory='templates')
+
+@app.get('/book/{id}', response_model=HTMLResponse)
+async def read(requests : Request, book : str, id : int):
+    return templates.TemplatesResponse('index.html', {'request' : requests, 'book' : book, 'id' : id})
