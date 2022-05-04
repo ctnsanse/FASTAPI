38bbbf57fb3907d8f3b1246fd4e3c8bb0d8da9d9
Ctnsanse <cleytonsanse@gmail.com>
2022-05-04 02:57:16 +0200

    test3

--- jinja.py
+++ jinja.py
@@ -10,3 +10,6 @@ templates = Jinja2Templates(directory='templates')
 @app.get('/book/{id}', response_model=HTMLResponse)
 async def read(requests : Request, book : str, id : int):
     return templates.TemplatesResponse('index.html', {'request' : requests, 'book' : book, 'id' : id})
+
+    #bruno
+    
